9745f27b1e8479019f497c85780e721e9fdbf1c9
Cengiz <cengiz69mg@gmail.com>
2023-08-31 16:50:27 +0300

    restful api get products and list

---
+++ WebProject/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebProject.Models;
+
+namespace WebProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        Uri baseAddress = new Uri("https://api.escuelajs.co/api/v1");
+        private readonly HttpClient _httpClient;
+        List<Product> products = new List<Product>();
+
+        public ProductController()
+        {
+            _httpClient= new HttpClient();
+            _httpClient.BaseAddress = baseAddress;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            HttpResponseMessage response = _httpClient.GetAsync(_httpClient.BaseAddress + "/products?offset=1&limit=12").Result;
+
+            if(response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result; 
+                products = JsonConvert.DeserializeObject<List<Product>>(data);
+            }
+
+            return View(products);
+        }
+
+        public IActionResult ProductDetail()
+        {
+            return View(products);
+        }
+    }
+}
---
+++ WebProject/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebProject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public string? Description { get; set; }
+        [Required]
+        public List<String>? Images { get; set; }
+
+    }
+}
